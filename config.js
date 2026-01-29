a3794dc71ad28350459c02151c1dfbb1fec6eace
CuddieMarc <mcgames033@gmail.com>
2026-01-28 22:17:16 -0800

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     // Add your own music URL after getting proper licenses
     music: {
         enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/donotmbug/video/upload/v1769666400/Clairo_-_Hello_feat._Rejjie_Snow__mp3.pm_aq4zen.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
